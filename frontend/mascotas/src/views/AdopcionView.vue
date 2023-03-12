3533f7c4faea1349c5dbad3ee4c066e682b41c14
David Ospina <J_do_o13@hotmail.com>
2023-03-11 21:13:30 -0500

    add table
    se añade tabla de mascotas en adopción

--- frontend/mascotas/src/views/AdopcionView.vue
+++ frontend/mascotas/src/views/AdopcionView.vue
@@ -79,6 +79,27 @@
             <ModalFotos tipo="asd" :fotos="fotos"/>
         </div>
     </div>
+    
+    <div class="row d-flex justify-content-center">
+        <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
+            <table class="table table-sm">
+                <thead class="table-light">
+                    <tr>
+                        <th>Foto</th>
+                        <th>Nombre</th>
+                        <th>Usuario</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><img src="https://www.eltiempo.com/files/article_vertical_content_new/uploads/2021/12/03/61aa91564695c.jpeg" class="imgp"></td>
+                        <td>Mona</td>
+                        <td>Oscar</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -251,4 +272,12 @@ input[type=number]::-webkit-outer-spin-button {
     -webkit-appearance: none;
     margin: 0;
 } 
+
+.imgp {
+    width: 70px;
+    height: 70px;
+    border-radius: 100%;
+}
+
+
 </style>
